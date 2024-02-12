d1e049cce832b71d83019891ceda1494df118401
Ikigai2004 <zloytigr2004@gmail.com>
2024-02-12 08:23:19 +0600

    version1.0

--- static/js/script.js
+++ static/js/script.js
@@ -97,38 +97,38 @@ var arrLang = {
 }
 
  $(function (){
-     $('.translate').click(function (){
-         var lang = $(this).attr('id');
-         saveLocalLang(lang)
+  $('.translate').click(function (){
+    var lang = $(this).attr('id');
+    saveLocalLang(lang)
 
-         $('.lang').each(function(index, item) {
-             $(this).text(arrLang[lang][$(this).attr('key')]);
-         })
-     })
+    $('.lang').each(function(index, item) {
+      $(this).text(arrLang[lang][$(this).attr('key')]);
+    })
+  })
  })
 
 function saveLocalLang(language) {
     let langs
     if (localStorage.getItem('langs') == null){
-        langs = []
+      langs = []
     } else {
-        langs = JSON.parse(localStorage.getItem('langs'))
+      langs = JSON.parse(localStorage.getItem('langs'))
     }
     langs.push(language)
     localStorage.setItem('langs', JSON.stringify(langs))
 }
 
 function getLocalLang() {
-    let langs
-    if (localStorage.getItem('langs') == null){
-        langs == []
-    } else{
-        langs = JSON.parse(localStorage.getItem('langs'))
-    }
-    langs.forEach(function (language) {
-        let lang = langs[langs.length - 1]
-        $('.lang').each(function (index, item) {
-            $(this).text(arrLang[lang][$(this).attr('key')]);
-        });
-    })
+  let langs
+  if (localStorage.getItem('langs') == null){
+    langs == []
+  } else{
+    langs = JSON.parse(localStorage.getItem('langs'))
+  }
+  langs.forEach(function (language) {
+    let lang = langs[langs.length - 1]
+    $('.lang').each(function (index, item) {
+      $(this).text(arrLang[lang][$(this).attr('key')]);
+    });
+  })
 }
