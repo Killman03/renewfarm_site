c68f95a0cd51aaa1b11a87ec97fdc6fccf4bc1fd
killman03 <tg3381234@gmail.com>
2024-02-17 13:24:14 +0600

    Version 1.0 (adaptive 17.02)

--- app.py
+++ app.py
@@ -15,40 +15,40 @@ mail = Mail(app)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', footer_text=[{'one': 'Contact us here for more information', 'two': 'inform'}])
 
 
 @app.route('/about', methods=["POST", "GET"])
 def about():
-    return render_template('about.html')
+    return render_template('about.html', footer_text = [{'one': 'Contact us here for more information', 'two': 'inform'}])
 
 
 @app.route('/pasture-Raised-Chicken', methods=["POST", "GET"])
 def chicken_pasture():
-    return render_template('chick.html')
+    return render_template('chick.html', footer_text = [{'one': 'Contact us here to place an order!', 'two': 'order'}])
 
 
 @app.route('/contact', methods=["POST", "GET"])
 def contacts():
-    return render_template('contacts.html')
+    return render_template('contacts.html', footer_text = [{'one': 'Contact us here for more information', 'two': 'inform'}])
 
 
 @app.route('/send_email', methods=['POST'])
 def send_email():
     name = request.form['name']
-    surname = request.form['surname']
+    phone_number = request.form['phone']
     email = request.form['email']
     comment = request.form['comment']
     subject = 'Subject_1'
     msg = Message(subject=subject, sender=email, recipients=['tg3381234@gmail.com'])
-    msg.body = comment
+    msg.body = f'Name: {name}\nPhone: {phone_number}\nEmail: {email}\nComment: {comment}'
     mail.send(msg)
     return 'Email sent seccessfully'
 
 
 @app.route('/farming-club')
 def farming_club():
-    return render_template('club.html')
+    return render_template('club.html', footer_text = [{'one': 'Contact us here to join our club', 'two': 'club-join'}])
 
 
 if __name__ == '__main__':
